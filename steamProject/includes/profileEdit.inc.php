26ce7b9acbf230f51b4d9b2b7f9dff8da3539bb2
Noah <33662924+Cracken211@users.noreply.github.com>
2023-06-08 23:46:31 +0200

    Update profileEdit.inc.php

--- steamProject/includes/profileEdit.inc.php
+++ steamProject/includes/profileEdit.inc.php
@@ -1,20 +1,23 @@
 <?php
 require("functions.inc.php");
 
+// Function to change the theme for a user
 function themeChange($theme)
 {
     global $pdo;
 
-    $id = $_SESSION["uid"];
+    $id = $_SESSION["uid"]; // Get the user ID from the session
 
+    // Update the user's theme in the database
     $statement = $pdo->prepare("UPDATE user SET theme = :theme WHERE id = :id");
     $statement->execute(["theme" => $theme, "id" => $id]);
 
 }
 
-if(!themeChange($_POST["theme"])){
-    header("location: /steamProject/profileEdit.php?message=Edit Success");
+// Call the themeChange function with the provided theme from the form
+if (!themeChange($_POST["theme"])) {
+    header("location: /steamProject/profileEdit.php?message=Edit Success"); // Theme change successful
 } else {
-    header("location: /steamProject/profileEdit.php?message=Edit Failed");
+    header("location: /steamProject/profileEdit.php?message=Edit Failed"); // Theme change failed
 }
 ?>
