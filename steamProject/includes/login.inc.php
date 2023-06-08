fbe9045e23269e84c869e7ee256693c4c1c7d693
Noah <33662924+Cracken211@users.noreply.github.com>
2023-06-08 23:45:35 +0200

    Update login.inc.php

--- steamProject/includes/login.inc.php
+++ steamProject/includes/login.inc.php
@@ -5,6 +5,7 @@ $username = $_POST["uid"];
 $password = $_POST["pass"];
 $email = $_POST["email"];
 
+// Function to check if user exists and validate password
 function checkUser($email, $password)
 {
     global $pdo;
@@ -18,9 +19,9 @@ function checkUser($email, $password)
     } else {
         if (password_verify($password, $user["password"])) {
             $_SESSION["uid"] = $user["id"];
-            return 1;
+            return 1; // password correct
         } else {
-            return 0;
+            return 0; // password incorrect
         }
     }
 }
@@ -29,7 +30,6 @@ $checkUserResult = checkUser($email, $password);
 if ($checkUserResult === -1) {
     header("location: ../login.php?message=User Not Found");
     echo "<p></p>";
-
 } elseif ($checkUserResult === 1) {
     header("location: ../index.php?");
     $_SESSION["email"] = $email;
@@ -39,5 +39,4 @@ if ($checkUserResult === -1) {
 } else {
     header("location: ../login.php?message=idk");
     echo "<p>idk</p>";
-
 }
