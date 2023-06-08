bc47957c0d93fcd8375e5bd7e597d247ef745176
Noah <33662924+Cracken211@users.noreply.github.com>
2023-06-08 23:48:03 +0200

    Update signup.inc.php

--- steamProject/includes/signup.inc.php
+++ steamProject/includes/signup.inc.php
@@ -1,6 +1,7 @@
 <?php
 include("functions.inc.php");
 
+// Function to create a new user
 function userCreate($username, $password, $email)
 {
     global $pdo;
@@ -9,24 +10,21 @@ function userCreate($username, $password, $email)
     $statement->execute(array(":username" => $username, ":email" => $email));
     $user = $statement->fetch();
 
-
     if ($user) {
-        return 0;
+        return 0; // User with the same username or email already exists
     } else {
-
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
         $statement = $pdo->prepare("INSERT INTO user(username, password, email) VALUES(:username, :password, :email);");
         if ($statement->execute(array(":username" => $username, ":password" => $hashedPassword, ":email" => $email))) {
-            // User was created
-            return 1;
+            return 1; // User created successfully
         } else {
-            // An error while trying to create the user
-            return -1;
+            return -1; // Error occurred while creating the user
         }
     }
 }
 
+// Redirect to index.php if the required form fields are not set
 if (!isset($_POST["uid"]) && !isset($_POST["pass"]) && !isset($_POST["email"]) && !isset($_POST["passrepeat"])) {
     header("location: ../index.php");
     exit();
@@ -41,17 +39,15 @@ if (str_contains($email, "@")) {
     if ($password != $passrepeat) {
         header("location: ../signup.php?message=Passwords Do Not Match");
     } else {
-
         $result = userCreate($username, $password, $email);
 
         if ($result == 0) {
             header("location: ../signup.php?message=Username Or Email Already in use");
         } else if ($result == -1) {
-            header("location: ../signup.php?message= An Error Occured");
+            header("location: ../signup.php?message=An Error Occurred");
         } else {
-            header("location: ../index.php?message=signup redirect"); // Fix else statament
+            header("location: ../index.php?message=signup redirect");
         }
-
     }
 } else {
     header("location: ../signup.php?message=Invalid Email");
