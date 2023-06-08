953db813a3f31647c48ca39f72f3fb19ba741e02
Noah <33662924+Cracken211@users.noreply.github.com>
2023-06-08 23:20:47 +0200

    Delete steamProject/includes directory

--- steamProject/includes/profileEdit.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("functions.inc.php");
-
-if(!themeChange($_POST["theme"])){
-    header("location: /steamProject/profileEdit.php?message=Edit Success");
-} else {
-    header("location: /steamProject/profileEdit.php?message=Edit Failed");
-}
-?>
