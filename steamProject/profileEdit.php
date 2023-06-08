d96a17f30eacfc99dcf221ff4eaa1d58c8a3344c
Noah <33662924+Cracken211@users.noreply.github.com>
2023-06-08 12:18:16 +0200

    Update profileEdit.php

--- steamProject/profileEdit.php
+++ steamProject/profileEdit.php
@@ -7,8 +7,10 @@ require_once("header.php");
 
 <div class="profileEdit-container">
   <h1>Edit Profile</h1>
+  
+  // gives users multiple choices, and later redirects to a link with the value for later use
   <form action="includes/profileEdit.inc.php" method="POST" enctype="multipart/form-data">
-
+ 
   <?php
   // if(isset($_GET["message"])){
   //   echo  "<p class='message-edit'>" . $_GET["message"] . "</p>" . "<style> .message-edit{ "; 
